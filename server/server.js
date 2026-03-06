cfa25b0e803d635fcb34ecb2b99ce20015104ba3
Tushar Dumra <tushardumra07@gmail.com>
2026-03-06 15:11:08 +0530

    added

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // app.use(cors());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://stelina-iep9.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
